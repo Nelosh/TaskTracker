cebb257c88153d5d107308a23637123cf7cdf694
Nelosh <asakura.jo@gmail.com>
2018-02-28 21:28:35 +0200

    Kotlin cheat

--- src/main/kotlin/lv/tti/TaskTracker/model/Task.kt
+++ src/main/kotlin/lv/tti/TaskTracker/model/Task.kt
@@ -5,5 +5,7 @@ import javax.persistence.Entity
 import javax.persistence.Id
 
 @Entity(name = "Task")
-data class Task(@Id @Column(name = "id")          var id: Long? = null,
-                    @Column(name = "description") var description: String? = null)
+data class Task(
+        @Id @Column(name = "id")          var id: Long,
+            @Column(name = "description") var description: String
+)
--- src/main/kotlin/lv/tti/TaskTracker/model/TaskRepository.kt
+++ src/main/kotlin/lv/tti/TaskTracker/model/TaskRepository.kt
@@ -1,10 +1,6 @@
 package lv.tti.TaskTracker.model
 
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.jdbc.core.BeanPropertyRowMapper
-import org.springframework.jdbc.core.JdbcTemplate
-import org.springframework.stereotype.Repository
 import javax.transaction.Transactional
 
 @Transactional(Transactional.TxType.MANDATORY)
